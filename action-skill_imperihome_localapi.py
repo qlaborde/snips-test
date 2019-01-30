5e71c60beb8cac41d7318386bddadd1705513570
qlaborde <qlaborde@evertygo.com>
2019-01-30 11:07:18 +0100

    test

--- action-skill_imperihome_localapi.py
+++ action-skill_imperihome_localapi.py
@@ -2,7 +2,7 @@
 # @Date:   2019-01-30T08:19:31+01:00
 # @Email:  qlaborde@evertygo.com
 # @Last modified by:   laborde
-# @Last modified time: 2019-01-30T08:50:12+01:00
+# @Last modified time: 2019-01-30T11:06:54+01:00
 
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
@@ -32,10 +32,16 @@ class ImperiHome(object):
         # action code goes here...
         print '[Received] intent: {}'.format(intent_message.intent.intent_name)
 
-        data = requests.get("http://192.168.10.185:8080/api/rest/imperihome/about").json();
+
+        IP =  self.config.get("global").get("IP")
+
+        # url = "http://192.168.10.185:8080/api/rest/imperihome/about"
+        url = "http://"+IP+":8080/api/rest/imperihome/about"
+
+        data = requests.get(url).json();
 
         # if need to speak the execution result by tts
-        hermes.publish_start_session_notification(intent_message.site_id, "About has been done : " + str(data.get("versionCode")), "")
+        hermes.publish_start_session_notification(intent_message.site_id, "Application code version : " + str(data.get("versionCode")), "")
 
     # --> Master callback function, triggered everytime an intent is recognized
     def master_intent_callback(self,hermes, intent_message):
