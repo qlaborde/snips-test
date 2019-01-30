fceabeab8df716ad342885514ef11abc222854ea
qlaborde <qlaborde@evertygo.com>
2019-01-30 13:15:17 +0100

    test

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #/usr/bin/env bash -e
 
-if [ ! -e "./config.ini" ]
-then
-    cp config.ini.default config.ini
-fi
+# @Author: laborde
+# @Date:   2019-01-30T08:47:10+01:00
+# @Email:  qlaborde@evertygo.com
+# @Last modified by:   laborde
+# @Last modified time: 2019-01-30T13:15:10+01:00
+
+# if [ ! -e "./config.ini" ]
+# then
+cp config.ini.default config.ini
+# fi
 
 
 VENV=venv
