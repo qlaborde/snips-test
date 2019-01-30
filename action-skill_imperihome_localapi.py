85ca65f8f20009e324619ea86814e3407a742e4e
qlaborde <qlaborde@evertygo.com>
2019-01-30 15:25:46 +0100

    test

--- action-skill_imperihome_localapi.py
+++ action-skill_imperihome_localapi.py
@@ -5,7 +5,7 @@
 # @Date:   2019-01-30T08:19:31+01:00
 # @Email:  qlaborde@evertygo.com
 # @Last modified by:   laborde
-# @Last modified time: 2019-01-30T14:19:08+01:00
+# @Last modified time: 2019-01-30T15:25:37+01:00
 
 from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
@@ -52,11 +52,28 @@ class ImperiHome(object):
         data = requests.get(url).json();
         hermes.publish_start_session_notification(intent_message.site_id, "I am a "+ str(data.get("device")) +". My version name is "+ str(data.get("versionName")) +" and my version code is " + str(data.get("versionCode")), "")
 
+    # Get app code version
+    def temp_callback(self, hermes, intent_message):
+        hermes.publish_end_session(intent_message.session_id, "")
+        print '[Received] intent: {}'.format(intent_message.intent.intent_name)
+
+        print('intent_message = ' + str(intent_message))
+
+        ip = self.config.get('secret').get('ip')
+        port = self.config.get('secret').get('port')
+
+        url = "http://"+ip+":"+port+"/api/rest/imperihome/about"
+        print('url = ' + url)
+        data = requests.get(url).json();
+        hermes.publish_start_session_notification(intent_message.site_id, "I am a "+ str(data.get("device")) +". My version name is "+ str(data.get("versionName")) +" and my version code is " + str(data.get("versionCode")), "")
+
     # --> Master callback function, triggered everytime an intent is recognized
     def master_intent_callback(self,hermes, intent_message):
         coming_intent = intent_message.intent.intent_name
         if coming_intent == 'evertygo:about':
             self.about_callback(hermes, intent_message)
+        if coming_intent == 'evertygo:temp':
+            self.temp_callback(hermes, intent_message)
 
     # --> Register callback function and start MQTT
     def start_blocking(self):
