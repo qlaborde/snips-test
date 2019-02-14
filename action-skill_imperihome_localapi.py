782b3eda29d2a25b0787f84f7718501245c6a56a
qlaborde <qlaborde@evertygo.com>
2019-02-14 12:50:47 +0100

    test

--- action-skill_imperihome_localapi.py
+++ action-skill_imperihome_localapi.py
@@ -108,8 +108,8 @@ class ImperiHome(object):
                 level = str(data.get("level").get("value"))
                 res = "The " + str(device_name) +" level is " + level + " %"
                 if type == 'shutter' :
-                    print('type(level) = ' + str(type(level)))
-                    print('level = ' + str(level))
+                    print('type(level) = ' + type(level))
+                    print('level = ' + level)
                     if level == 0:
                         res = str(device_name) +" is close"
                     elif level >= 100:
