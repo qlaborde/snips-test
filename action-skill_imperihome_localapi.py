a787e92027db4ab273d6a6fee6dbfdf10059674c
qlaborde <qlaborde@evertygo.com>
2019-02-28 15:41:20 +0100

    test

--- action-skill_imperihome_localapi.py
+++ action-skill_imperihome_localapi.py
@@ -63,7 +63,7 @@ class ImperiHome(object):
             elif data != None and 'error' in data:
                 hermes.publish_end_session(intent_message.session_id, str(data.get("error").get("message")))
             else:
-                print('device_name = ' + device_name)
+                print('device_name = ' + str(device_name))
                 hermes.publish_end_session(intent_message.session_id, "Sorry, I can't get the device temperature 1")
         except Exception as e:
             print('e = ' + str(e))
