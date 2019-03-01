bcc1dd3fe01bba67656c4dc333e59c0574acd7b0
qlaborde <qlaborde@evertygo.com>
2019-03-01 08:46:21 +0100

    test

--- action-skill_imperihome_localapi.py
+++ action-skill_imperihome_localapi.py
@@ -218,7 +218,7 @@ class ImperiHome(object):
             print("type url = " + str(type(url)))
 
             print('dfbvrbrnbfg')
-            print('url = ' + str(url))
+            print("url = " + url)
             data = requests.get(url, timeout=2).json()
             print('data = ' + str(data))
             return data
