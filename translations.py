6acf806d23f9032a17bbd232e575b9dfc429dd6b
qlaborde <qlaborde@evertygo.com>
2019-03-05 11:19:25 +0100

    test

---
+++ translations.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+translations = {
+    "en":{
+        "test":"english",
+        "error_temp_data":"Sorry, I can't get the temperature for this device",
+        "error_temp_unknown":"Sorry, an error has occurred",
+        "error_hum_data":"Sorry, I can't get the humidity for this device",
+        "error_hum_unknown":"Sorry, an error has occurred",
+        "error_status_data":"Sorry, I can't get the status for this device",
+        "error_status_unknown":"Sorry, an error has occurred",
+        "error_level_data":"Sorry, I can't get the level for this device",
+        "error_level_unknown":"Sorry, an error has occurred",
+        "error_set_status":"Sorry, I can't set the status for this device",
+        "error_set_status_unknown":"Sorry, an error has occurred",
+        "error_set_level":"Sorry, I can't set the level for this device",
+        "error_set_level_unknown":"Sorry, an error has occurred",
+        "error_set_color":"Sorry, I can't set the color for this device",
+        "error_set_color_unknown":"Sorry, an error has occurred",
+        "error_set_position":"Sorry, I can't set the position for this device",
+        "error_set_position_unknown":"Sorry, an error has occurred"
+    },
+    "fr":{
+        "test":"français",
+        "error_temp_data":"Désolé, je n'arrive pas à récuperer la température de cet appareil.",
+        "error_temp_unknown":"Désolé, une erreur s'est produite",
+        "error_hum_data":"Désolé, je n'arrive pas à récuperer la valeur de l'humidité pour cet appareil.",
+        "error_hum_unknown":"Désolé, une erreur s'est produite",
+        "error_status_data":"Désolé, je n'arrive pas à récuperer la statut de cet appareil.",
+        "error_status_unknown":"Désolé, une erreur s'est produite",
+        "error_level_data":"Désolé, je n'arrive pas à récuperer le niveau pour cet appareil.",
+        "error_level_unknown":"Désolé, une erreur s'est produite",
+        "error_set_status":"Désolé, je ne peux pas modifier le statut de cet appareil.",
+        "error_set_status_unknown":"Désolé, une erreur s'est produite",
+        "error_set_level":"Désolé, je ne peux pas modifier le niveau de cet appareil.",
+        "error_set_level_unknown":"Désolé, une erreur s'est produite",
+        "error_set_color":"Désolé, je ne peux pas modifier la couleur de cet appareil.",
+        "error_set_color_unknown":"Désolé, une erreur s'est produite",
+        "error_set_position":"Désolé, je ne peux pas modifier la position de cet appareil.",
+        "error_set_position_unknown":"Désolé, une erreur s'est produite"
+    }
+}
