c5777e03ce2f0c1e0549424e83d53a83e985140e
qlaborde <qlaborde@evertygo.com>
2019-03-05 10:41:22 +0100

    test

--- action-skill_imperihome_localapi.py
+++ action-skill_imperihome_localapi.py
@@ -67,7 +67,7 @@ class ImperiHome(object):
         print('url = ' + url)
         data = requests.get(url).json();
         # hermes.publish_end_session(intent_message.session_id, "I am a "+ str(data.get("device")) +". My version name is "+ str(data.get("versionName")) +" and my version code is " + str(data.get("versionCode")))
-        hermes.publish_end_session(intent_message.session_id, resp['fr']['test'])
+        hermes.publish_end_session(intent_message.session_id, self.t['fr']['test'])
 
     def getTemp_callback(self, hermes, intent_message):
         # hermes.publish_end_session(intent_message.session_id)
